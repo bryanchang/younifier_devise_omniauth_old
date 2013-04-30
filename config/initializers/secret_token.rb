f6a7d6f09097f5f3b9038ee8e89f1a339e1f868d
Bryan Chang <keunyoung.chang@gmail.com>
2013-04-29 21:49:31 -0700

    Younifier_v2 first commit - Devise integrated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Younifier::Application.config.secret_token = 'a14d90a6eb87652320ebc29a0b265ada8ae12f2253742dfa9ea3b7a94249628aed2fae69784077ea10a2d5da4d38d232b97f8ab794a1778d72cd3f2574bdae52'
